4ee281c24559929fd8928441f2eedb995ca1bad0
wahyutyan87 <96811665+wahyutyan87@users.noreply.github.com>
2023-12-28 15:06:56 +0700

    Add files via upload

---
+++ application/controllers/Hello.php
@@ -0,0 +1,11 @@
+<?php
+class Hello extends CI_Controller{
+
+    function index(){
+        echo "Hello World";
+    }
+
+    function show(){
+        echo " I make The WMP Better place.";
+    }
+}
---
+++ application/controllers/dashboard.php
@@ -0,0 +1,7 @@
+<?php
+class Dashboard Extends CI_Controller{
+    public function index()
+    {
+        $this->load->view('v_dashboard');
+    }
+}
